6a7c3c9e577997cabbe49a25993c1767b1805b10
CyskoFR <franc.sko91@gmail.com>
2022-05-02 16:10:50 +0200

    fix fizzbuzz

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ for( let i = 1; i <= 100; i++ ) {
     element.classList.add("item");
     element.innerText = i;
     list.append(element);
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        element.innerText = "FizzBuzz"
+        element.style.backgroundColor = "#f0466f"
+    }   else if (i % 5 === 0) {
+        element.innerText = "Buzz"
+        element.style.backgroundColor = "#ffd166"
+    }   else if (i % 3 === 0) {
         element.innerText = "Fizz"
         element.style.backgroundColor = "#0bd6a1"
-    }   else if (i % 5 === 0) {
-            element.innerText = "Buzz"
-            element.style.backgroundColor = "#ffd166"
-        }   else if (i % 3 === 0 && i % 5 === 0) {
-            element.innerText = "FizzBuzz"
-            element.style.backgroundColor = "#f0466f"
-            }
+    }
 }
 
