ab0ce2c8a26c7bd758c50db1b11b27a83734d1b0
CyskoFR <franc.sko91@gmail.com>
2022-05-02 15:57:36 +0200

    fizzbuzz

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const list = document.querySelector(".list");
+
+for( let i = 1; i <= 100; i++ ) {
+    const element = document.createElement("li");
+
+    element.classList.add("item");
+    element.innerText = i;
+    list.append(element);
+    if (i % 3 === 0) {
+        element.innerText = "Fizz"
+        element.style.backgroundColor = "#0bd6a1"
+    }   else if (i % 5 === 0) {
+            element.innerText = "Buzz"
+            element.style.backgroundColor = "#ffd166"
+        }   else if (i % 3 === 0 && i % 5 === 0) {
+            element.innerText = "FizzBuzz"
+            element.style.backgroundColor = "#f0466f"
+            }
+}
+
